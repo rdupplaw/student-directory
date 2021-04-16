e335ded67f03f6ead0a5e86e796bba87b6084044
Robert Dupplaw <dupplawrobert@gmail.com>
2021-04-16 12:16:04 +0000

    print_students groups by cohort

--- directory.rb
+++ directory.rb
@@ -48,10 +48,15 @@ end
 
 def print(students)
   filter_char = "H"
-  i = 0
-  while i < students.length do
-    puts "#{i}. #{students[i][:name]} (#{students[i][:cohort]} cohort)".center($COLUMN_WIDTH) if students[i][:name].chr == filter_char && students[i][:name].length < 12 
-    i += 1 
+  cohorts = students.reduce([]) do |acc, student|
+    acc.push(student[:cohort]) 
+  end
+
+  cohorts.each do |cohort|
+    puts "Cohort: #{cohort}".center($COLUMN_WIDTH)
+    students.each do |student|
+      puts student[:name].center($COLUMN_WIDTH) if student[:cohort] == cohort
+    end
   end
 end
 
