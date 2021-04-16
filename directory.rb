6da9d67b5cce6c5887a24c6534e22fb0e78620fb
Robert Dupplaw <dupplawrobert@gmail.com>
2021-04-16 11:17:23 +0000

    centered printed lines

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+$COLUMN_WIDTH = 50
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -23,21 +25,21 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "--------------"
+  puts "The students of Villains Academy".center($COLUMN_WIDTH)
+  puts "--------------".center($COLUMN_WIDTH)
 end
 
 def print(students)
   filter_char = "H"
   i = 0
   while i < students.length do
-    puts "#{i}. #{students[i][:name]} (#{students[i][:cohort]} cohort)" if students[i][:name].chr == filter_char && students[i][:name].length < 12
-    i += 1
+    puts "#{i}. #{students[i][:name]} (#{students[i][:cohort]} cohort)".center($COLUMN_WIDTH) if students[i][:name].chr == filter_char && students[i][:name].length < 12 
+    i += 1 
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center($COLUMN_WIDTH)
 end
 
 students = input_students
