1dbd566040c9bc257aa3f2c69cf93eb34be9176d
Robert Dupplaw <dupplawrobert@gmail.com>
2021-04-20 11:03:05 +0000

    created add_student method to add student to students array

--- directory.rb
+++ directory.rb
@@ -45,7 +45,7 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    @students << {name: name, cohort: :november}
+    add_student(name, :november)
     puts "Now we have #{@students.count} students"
     # get another name from the user
     name = STDIN.gets.chomp
@@ -68,9 +68,13 @@ def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_sym}
+    add_student(name, cohort.to_sym)
   end
   file.close
+end
+
+def add_student(name, cohort)
+  @students << {name: name, cohort: cohort}
 end
 
 def print_header
